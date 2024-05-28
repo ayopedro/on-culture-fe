8e723b8c4d2b7665efadf64d21976be66a6ada46
Ayotunde <ayopedro2012@gmail.com>
2024-05-28 22:43:13 +0100

    setup part 2

---
+++ app/utils/custom-table-styles.ts
@@ -0,0 +1,37 @@
+import { TableStyles } from 'react-data-table-component';
+
+export const customStyles: TableStyles = {
+  table: {
+    style: {
+      height: 'fit-content',
+    },
+  },
+  headRow: {
+    style: {
+      display: 'flex',
+      gap: '0.5rem',
+    },
+  },
+  headCells: {
+    style: {
+      padding: '0',
+      letterSpacing: '0.2px',
+      fontSize: '14px',
+    },
+  },
+  rows: {
+    style: {
+      display: 'flex',
+      gap: '0.5rem',
+    },
+  },
+  cells: {
+    style: {
+      padding: '0',
+      letterSpacing: '0.2px',
+      fontSize: '14px',
+      minWidth: 'unset',
+      textTransform: 'capitalize',
+    },
+  },
+};
