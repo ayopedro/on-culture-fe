b47535da75a6e4dfeca6bcc8447ff6bc86b915d7
Ayotunde <ayopedro2012@gmail.com>
2024-05-29 03:00:29 +0100

    sidebar completion

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { SideBarLinks } from '@@/utils/side-bar-links';
+import { SideBarLinks } from '@@/components/side-bar-links';
+import Logout from '@@/components/icons/logout';
+import Gear from '@@/components/icons/gear';
 import Link from 'next/link';
 
 type Props = {
@@ -9,17 +11,37 @@ type Props = {
 
 const Sidebar = ({ showSideBar }: Props) => {
   return (
-    <aside className={`${showSideBar ? 'col-span-2' : 'col-span-1'} p-5`}>
-      <ul className='flex flex-col gap-8'>
+    <aside
+      className={`${
+        showSideBar ? 'col-span-2' : 'col-span-1'
+      } p-5 flex flex-col justify-between`}
+    >
+      <ul
+        className={`flex flex-col ${
+          showSideBar ? 'items-start gap-8' : 'items-center gap-12'
+        }`}
+      >
         {SideBarLinks.map((item) => {
           return (
             <li key={item.key}>
               <Link href={item.url} className='flex gap-4 items-center'>
-                {item.icon} {item.title}
+                {item.icon} {showSideBar && item.title}
               </Link>
             </li>
           );
         })}
+      </ul>
+      <ul
+        className={`flex flex-col ${
+          showSideBar ? 'items-start gap-8' : 'items-center gap-12'
+        } mb-10`}
+      >
+        <li className='flex gap-4 items-center cursor-pointer'>
+          <Gear /> Settings
+        </li>
+        <li className='flex gap-4 items-center cursor-pointer'>
+          <Logout /> Log Out
+        </li>
       </ul>
     </aside>
   );
